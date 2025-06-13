34649e8d2b5423a7a709edf80c0e13d9ae7d8d8b
NBspatnaz <nejumbutex@outlook.com>
2025-06-14 04:45:31 +0600

    4_top_skills_based_on_salary.sql

---
+++ project_sql/4_top_skill_based_on_salary.sql
@@ -0,0 +1,32 @@
+/* what are the top skills based on slary for the data analyst role */
+
+WITH job AS (SELECT
+    job_id,
+    job_title_short,
+    salary_year_avg
+
+FROM job_postings_fact
+
+WHERE job_title_short = 'Data Analyst'
+AND salary_year_avg IS NOT NULL
+AND job_work_from_home = 'Yes'
+
+ORDER BY
+    salary_year_avg DESC)
+
+SELECT
+    skills_job.skill_id,
+    skills_dim.skills AS skills_name,
+    job.job_title_short,
+    ROUND (AVG(job.salary_year_avg),2) AS average_salary
+FROM skills_job_dim AS skills_job
+INNER JOIN job ON skills_job.job_id = job.job_id
+INNER JOIN skills_dim ON skills_job.skill_id = skills_dim.skill_id
+GROUP BY
+    skills_job.skill_id,
+    skills_dim.skills,
+    job.job_title_short
+ORDER BY
+    average_salary DESC
+
+LIMIT 25;
